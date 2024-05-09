796eb71612c2a16504ac5096b7f1b8c5d51a0222
FranAM1 <franciscojosealmansa05@gmail.com>
2024-05-09 21:41:52 +0200

    Creacion clase carta

---
+++ game/card.py
@@ -0,0 +1,41 @@
+from . import SUITS, RANKS
+class Card:
+    __suit: str
+    __rank: str
+
+    def __init__(self, suit: str, rank: str):
+        self.set_suit(suit)
+        self.set_rank(rank)
+
+    def __str__(self) -> str:
+        card = (
+        '┌─────────┐\n'
+        '│ {}      │\n'
+        '│         │\n'
+        '│    {}   │\n'
+        '│         │\n'
+        '│      {} │\n'
+        '└─────────┘\n'
+        ).format(
+            format(self.__rank, ' <2'),
+            format(self.__suit, ' <2'),
+            format(self.__rank, ' >2')
+        )
+        return card
+    
+    
+    def get_suit(self) -> str:
+        return self.__suit
+    
+    def get_rank(self) -> str:
+        return self.__rank
+    
+    def set_suit(self, suit: str):
+        if suit not in SUITS:
+            raise ValueError(f"Invalid suit: {suit}")
+        self.__suit = SUITS[suit]
+
+    def set_rank(self, rank: str):
+        if rank not in RANKS:
+            raise ValueError(f"Invalid rank: {rank}")
+        self.__rank = RANKS[rank]
---
+++ main.py
@@ -0,0 +1,7 @@
+from game.card import Card
+
+card = Card('Hearts', '11')
+card2 = Card('Diamonds', '1')
+card3 = Card('Clubs', '13')
+card4 = Card('Spades', '10')
+print(card, card2, card3, card4, sep='\n')
