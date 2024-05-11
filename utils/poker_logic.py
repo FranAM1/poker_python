589f13e3b7aeafc8d5e763ba0669258187b0bf77
FranAM1 <franciscojosealmansa05@gmail.com>
2024-05-11 15:27:29 +0200

    logica de posibles manos

---
+++ utils/poker_logic.py
@@ -0,0 +1,76 @@
+from game import SUITS, RANKS, HANDS_RANKING
+
+def is_royal_flush(hand: list) -> bool:
+    return is_straight_flush(hand) and hand[0].get_rank() == "10"
+
+def is_straight_flush(hand: list) -> bool:
+    return is_straight(hand) and is_flush(hand)
+
+def is_four_of_a_kind(hand: list) -> bool:
+    ranks = [card.get_rank() for card in hand]
+    for rank in ranks:
+        if ranks.count(rank) == 4:
+            return True
+    return False
+
+def is_full_house(hand: list) -> bool:
+    return is_three_of_a_kind(hand) and is_one_pair(hand)
+
+def is_flush(hand: list) -> bool:
+    suits = [card.get_suit() for card in hand]
+    return len(set(suits)) == 1
+
+def is_straight(hand: list) -> bool:
+    ranks = [card.get_rank() for card in hand]
+    ranks_values = [RANKS[rank] for rank in ranks]
+    ranks_values.sort()
+    return ranks_values == list(range(ranks_values[0], ranks_values[0] + 5))
+
+def is_three_of_a_kind(hand: list) -> bool:
+    ranks = [card.get_rank() for card in hand]
+    for rank in ranks:
+        if ranks.count(rank) == 3:
+            return True
+    return False
+
+def is_two_pair(hand: list) -> bool:
+    ranks = [card.get_rank() for card in hand]
+    pairs = 0
+    for rank in ranks:
+        if ranks.count(rank) == 2:
+            pairs += 1
+    return pairs == 2
+
+def is_one_pair(hand: list) -> bool:
+    ranks = [card.get_rank() for card in hand]
+    for rank in ranks:
+        if ranks.count(rank) == 2:
+            return True
+    return False
+
+HAND_VALUES = {
+    "royal_flush": is_royal_flush,
+    "straight_flush": is_straight_flush,
+    "four_of_a_kind": is_four_of_a_kind,
+    "full_house": is_full_house,
+    "flush": is_flush,
+    "straight": is_straight,
+    "three_of_a_kind": is_three_of_a_kind,
+    "two_pair": is_two_pair,
+    "one_pair": is_one_pair
+}
+
+def value_of_hand(hand:dict, board:dict):
+    
+    full_hand = hand + board
+    value = "high_card"
+
+    for hand_value, function in HAND_VALUES.items():
+        if function(full_hand):
+            value = hand_value
+            break
+
+    return HANDS_RANKING[value]
+
+
+    
