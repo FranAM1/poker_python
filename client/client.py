78a0c9ad57eda37ffac363cd84f67ef904ab95e0
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2024-05-19 19:39:26 +0200

    eliminar cola

--- client/client.py
+++ client/client.py
@@ -2,23 +2,11 @@ import socket
 import threading
 import json
 from . import actions
-import queue
 class PokerClient:
     def __init__(self, host='localhost', port=12345):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((host, port))
-        self.output_queue = queue.Queue()
         print(f'Conectado al servidor{host}:{port}')
-
-    def print_message(self, message):
-        self.output_queue.put(message)
-
-    def handle_output(self):
-        while True:
-            message = self.output_queue.get()
-            if message is None:
-                break
-            print(message)
 
     def receive_messages(self):
         while True:
@@ -28,16 +16,16 @@ class PokerClient:
                     data = json.loads(message.decode())
 
                     if "pot" in data:
-                        self.print_message(f'Bote actualizado: {data["pot"]}')
+                        print(f'Bote actualizado: {data["pot"]}')
                     if "player_chips" in data:
                         player_chips = data["player_chips"]
-                        self.print_message(f'El jugador {player_chips["name"]} ha apostado {player_chips["bet"]} fichas. Fichas restantes: {player_chips["chips"]}')
+                        print(f'El jugador {player_chips["name"]} ha apostado {player_chips["bet"]} fichas. Fichas restantes: {player_chips["chips"]}')
                     if "voted" in data:
-                        self.print_message(data["voted"])
+                        print(data["voted"])
                     if "game_started" in data:
-                        self.print_message(data["game_started"])
+                        print(data["game_started"])
                     if "game_not_started" in data:
-                        self.print_message(data["game_not_started"])
+                        print(data["game_not_started"])
                     
                 else:
                     break
@@ -50,7 +38,6 @@ class PokerClient:
 
     def start(self):
         threading.Thread(target=self.receive_messages).start()
-        threading.Thread(target=self.handle_output).start()
 
 def run_client():
     client = PokerClient()
@@ -63,9 +50,9 @@ def run_client():
             action = input('Escribe una accion (ver todas con "acciones"): ')
             
             if action == "acciones":
-                client.print_message('Acciones disponibles:')
+                print('Acciones disponibles:')
                 for action in actions:
-                    client.print_message(action)
+                    print(action)
             elif action == "votar":
                 client.send_message({"action": action})
             elif action == "apuesta":
@@ -73,7 +60,7 @@ def run_client():
                 client.send_message({"action": action, "amount": amount})
             elif action == "salir":
                 client.send_message({"action": action})
-                client.print_message("Desconectándose del servidor...")
+                print("Desconectándose del servidor...")
                 break
             else:
-                client.print_message('Accion no tratada. Escribe "acciones" para ver las acciones disponibles.')
+                print('Accion no tratada. Escribe "acciones" para ver las acciones disponibles.')
